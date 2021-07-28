89597ea3b28f2c262e9e956f4684cd06156ae03d
gauravmishra1076 <gaurav.mishra1@srijan.net>
2021-07-28 20:18:50 +0530

    Initial Commit

---
+++ LoginApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginApp
+//
+//  Created by gaurav.mishra on 28/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by gaurav.mishra on 28/07/21.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
